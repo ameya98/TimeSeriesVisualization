fc1fd0f70fac6937bc65948a05beaa91999cb5ce
Ameya Daigavane <ameya.s.daigavane@jpl.nasa.gov>
2019-06-18 16:59:09 -0700

    Added doctests for MDL functions.

--- tsvisualize/visualization.py
+++ tsvisualize/visualization.py
@@ -18,16 +18,48 @@ from sklearn.manifold import MDS
 class MDL:
     @staticmethod
     def discretize(subsequence, num_bits):
+        """
+        :param subsequence: Subsequence to be discretized.
+        :param num_bits: Number of bits used to represent each bit of the discretized subsequence.
+        :return: Discretized subsequence.
+
+        >>> s1 = np.array([1, 3, 4, 5, 4, 3, 6, 15, 14, 13, 12, 0, 2, 4, 2, 6, 10, 11, 9, 3])
+        >>> '%s' % MDL.discretize(s1, num_bits=4) # doctest: +NORMALIZE_WHITESPACE
+        '[ 2 4 5 6 5 4 7 16 15 14 13 1 3 5 3 7 11 12 10 4]'
+        >>> s2 = np.array([1, 3, 4, 5, 3])
+        >>> '%s' % MDL.discretize(s2, num_bits=4) # doctest: +NORMALIZE_WHITESPACE
+        '[ 1 9 12 16 9]'
+        """
         subsequence_min = np.min(subsequence)
         subsequence_max = np.max(subsequence)
         return np.round((subsequence - subsequence_min) / (subsequence_max - subsequence_min)).astype(int) * (2 ** num_bits - 1) + 1
 
     @staticmethod
-    def description_length(sequence, num_bits):
-        return sequence.shape[0] * num_bits
+    def description_length(subsequence, num_bits):
+        """
+        :param subsequence: Subsequence - can be thought of as a vector of elements each represented with some number of bits.
+        :param num_bits: Number of bits used to represent each element in this subsequence.
+        :return: The description length of the subsequence. This corresponds to the space required to store this sequence uncompressed.
+
+        >>> s1 = np.array([1, 3, 4, 5, 4, 3, 6, 15, 14, 13, 12, 0, 2, 4, 2, 6, 10, 11, 9, 3])
+        >>> '%0.2f' % MDL.description_length(s1, num_bits=4)
+        '80.00'
+        """
+        return subsequence.shape[0] * num_bits
 
     @staticmethod
     def reduced_description_length(compressible_sequence, hypothesis_sequence, num_bits):
+        """
+        :param compressible_sequence: The subsequence we want to compress by representing it with the hypothesis subsequence.
+        :param hypothesis_sequence: The subsequence we will use to compress the other sequence by noting dissimilarities.
+        :param num_bits: The number of bits used to store each element in the compressed format.
+        :return: The reduced description length of the subsequence. This corresponds to the space required to store the compressible sequence with the hypothesis sequence.
+
+        >>> s1 = np.array([1, 3, 4, 5, 4, 3, 6, 15, 14, 13, 12, 0, 2, 4, 2, 6, 10, 11, 9, 3])
+        >>> s2 = np.array([1, 3, 3, 5, 4, 3, 6, 15, 14, 13, 12, 0, 2, 3, 2, 6, 4, 3, 1, 0])
+        >>> '%0.2f' % MDL.reduced_description_length(s1, s2, num_bits=4)
+        '49.93'
+        """
         return np.where((compressible_sequence != hypothesis_sequence))[0].size * (np.log2(compressible_sequence.shape[0]) + num_bits)
 
 
